42ca7f5c32695aae01b37d7c60c5a04c9bac3747
anunay777 <45771163+anunay777@users.noreply.github.com>
2020-05-08 06:20:24 +0530

    Update run_aclImdbSimple_pretrained_mixed.sh

--- run_aclImdbSimple_pretrained_mixed.sh
+++ run_aclImdbSimple_pretrained_mixed.sh
@@ -16,5 +16,5 @@ python w2v.py --input ${OUT}/data --save_data "demo" --embeddings "vectors_aclIm
 python main.py --corpus aclImdb_tok --model LSTMEncoder --debug \
 --multi_gpu --input ${OUT}/data --output_path ${OUT}/model --exp_name "aclImdbSimple_pretrained_mixed" \
 --use_pretrained_embeddings --nepoch_no_imprv 20 --timedistributed --d_hidden 512 --nepochs 50 \
---optim adam --beta1 0.0 --beta2 0.98 \
---inc_unlabeled_loss --wbatchsize 3000 --wbatchsize_unlabel 3000 --lambda_at 1.0 --lambda_vat 1.0 --lambda_entropy 1.0
+--optim adam --beta1 0.0 --beta2 0.9998 \
+--inc_unlabeled_loss --wbatchsize 3000 --wbatchsize_unlabel 3000 --lambda_at 1.0 --lambda_vat 1.0 
